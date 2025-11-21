bbfd19f9b227356a83f6304a550476d2fdf5b19f
Paiboon Panusbordee <paiboonpa@gmail.com>
2025-11-21 22:09:42 +0700

    upgrade to be able to run

--- react-example/src/index.js
+++ react-example/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'antd/dist/antd.min.css';
 import App from './App';
@@ -7,11 +7,11 @@ import { BrowserRouter } from 'react-router-dom'
 import store from './redux/store/store'
 import { Provider } from 'react-redux'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root'),
+  </Provider>
 );
--- react-example/src/pages/authentication/Login.js
+++ react-example/src/pages/authentication/Login.js
@@ -1,85 +1,92 @@
 import React from 'react'
-import { Row, Col, Form, Icon, Input, Button } from 'antd';
+import { Row, Col, Form, Input, Button } from 'antd';
 import logo from '../../images/logo.png'
 import { connect } from 'react-redux'
 import { login } from '../../redux/actions/actions'
 import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.form.validateFieldsAndScroll(async (err, values) => {
-      if (!err) {
-        const formData = new FormData();
-        formData.append('login_username', document.getElementById('login_username').value);
-        formData.append('login_password', document.getElementById('login_password').value);
-        try {
-          const response = await fetch('http://localhost:8080/Cors/demo', {
-            method: 'POST',
-            body: formData,
-            credentials: 'include'
-          })
-          alert(await response.text());
-        } catch (e) {
-          console.error(e);
-        }
-      }
-    });
+
+  formRef = React.createRef();
+
+  handleSubmit = async (values) => {
+    const formData = new FormData();
+    formData.append('login_username', values.username);
+    formData.append('login_password', values.password);
+
+    try {
+      const response = await fetch('http://localhost:8080/Cors/demo', {
+        method: 'POST',
+        body: formData,
+        credentials: 'include'
+      })
+      alert(await response.text());
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   render() {
-    const { getFieldDecorator } = this.props.form;
-
     return (
       <div>
-        <Row type="flex" justify="center" align="middle" style={{ height: '100vh' }}>
+        <Row justify="center" align="middle" style={{ height: '100vh' }}>
           <Col md={8} sm={12} xs={24}>
-            <img src={logo} alt="Logo Fakebook" style={{ width: '100%', paddingLeft: '24px', paddingRight: '24px', maxWidth: '400px' }}></img>
+            <img 
+              src={logo}
+              alt="Logo Fakebook"
+              style={{ width: '100%', paddingLeft: '24px', paddingRight: '24px', maxWidth: '400px' }}
+            />
           </Col>
+
           <Col md={8} sm={12} xs={24}>
-            <Form onSubmit={this.handleSubmit} className="login-form" style={{ maxWidth: '400px', width: '100%' }}>
-              <Form.Item label="Username">
-                {getFieldDecorator('username', {
-                  rules: [
-                    {
-                      required: true,
-                      message: 'Please input your nickname!'
-                    }
-                  ],
-                })(<Input />)}
+            <Form
+              ref={this.formRef}
+              onFinish={this.handleSubmit}
+              className="login-form"
+              style={{ maxWidth: '400px', width: '100%' }}
+              layout="vertical"
+            >
+
+              <Form.Item
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: 'Please input your nickname!' }]}
+              >
+                <Input />
               </Form.Item>
-              <Form.Item label="Password">
-                {getFieldDecorator('password', {
-                  rules: [
-                    {
-                      required: true,
-                      message: 'Please input your password!',
-                    }
-                  ],
-                })(<Input.Password />)}
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+              >
+                <Input.Password />
               </Form.Item>
+
               <Row>
                 <Col span={12}>
                   <Form.Item>
                     <Link to='/signup'>
-                      <Button block type="link" >
+                      <Button block type="link">
                         Signup
                       </Button>
                     </Link>
                   </Form.Item>
                 </Col>
+
                 <Col span={12}>
                   <Form.Item>
-                    <Button block type="primary" htmlType="submit" className="login-form-button">
+                    <Button block type="primary" htmlType="submit">
                       Log in
                     </Button>
                   </Form.Item>
                 </Col>
               </Row>
+
             </Form>
           </Col>
-        </Row >
-      </div >
+        </Row>
+      </div>
     )
   }
 }
@@ -88,5 +95,4 @@ const mapDispatchToProps = {
   login: login
 }
 
-const LoginForm = Form.create({ name: 'login' })(Login);
-export default connect(null, mapDispatchToProps)(LoginForm)
+export default connect(null, mapDispatchToProps)(Login);
